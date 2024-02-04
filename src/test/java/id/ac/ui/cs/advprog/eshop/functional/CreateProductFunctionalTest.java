adab3f8daf38f4432fd0009827ceefde00ca0f15
Dwiky Ahmad Megananta <dwikyahmad@gmail.com>
2024-02-04 20:38:43 +0700

    Deleted Unused import

--- src/test/java/id/ac/ui/cs/advprog/eshop/functional/CreateProductFunctionalTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/functional/CreateProductFunctionalTest.java
@@ -11,8 +11,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
-import dev.failsafe.internal.util.Assert;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
