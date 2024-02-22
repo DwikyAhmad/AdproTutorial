4959700ec21715f4862fb806d0b48a39558007b1
Dwiky Ahmad Megananta <dwikyahmad@gmail.com>
2024-02-22 20:53:36 +0700

    Implemented OCP to extend the behaviour of create method

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -3,32 +3,19 @@ package id.ac.ui.cs.advprog.eshop.repository;
 import id.ac.ui.cs.advprog.eshop.model.Product;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 
 @Repository
-public class ProductRepository {
-    private List<Product> productData = new ArrayList<>();
-
-    public Product create(Product product) {
-        productData.add(product);
-        return product;
-    }
-
-    public Iterator<Product> findAll() {
-        return productData.iterator();
-    }
+public class ProductRepository extends DataRepository<Product>{
 
     public Product findProduct(int index) {
-        return productData.get(index);
+        return getListData().get(index);
     }
 
     public Product editProduct(int index, Product product) {
-        return productData.set(index, product);
+        return getListData().set(index, product);
     }
 
     public Product deleteProduct(int index) {
-        return productData.remove(index);
+        return getListData().remove(index);
     }
 }
