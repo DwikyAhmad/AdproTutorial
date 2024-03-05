714abb25317ee1fb319d505a48b8810030898319
Dwiky Ahmad Megananta <dwikyahmad@gmail.com>
2024-03-05 09:32:40 +0700

    [RED] Add OrderRepository skeleton

--- src/test/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepositoryTest.java
@@ -55,7 +55,7 @@ public class OrderRepositoryTest {
     void testSaveUpdate() {
         Order order = orders.get(1);
         orderRepository.save(order);
-        Order newOrder = new Order(order.getId(), order.getProducts() order.getOrderTime(),
+        Order newOrder = new Order(order.getId(), order.getProducts(), order.getOrderTime(),
                 order.getAuthor(), OrderStatus.SUCCESS.getValue());
         Order result = orderRepository.save(newOrder);
 
